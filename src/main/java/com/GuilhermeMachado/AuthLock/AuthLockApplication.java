790b918818ed3a7a600ad2ba7db9b873d9c1ca73
Guilherme Machado-ISEP <1240713@isep.ipp.pt>
2025-10-15 22:00:18 +0100

    Changed the original repository creation method with a spring boot one

---
+++ src/main/java/com/GuilhermeMachado/AuthLock/AuthLockApplication.java
@@ -0,0 +1,13 @@
+package com.GuilhermeMachado.AuthLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthLockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GuilhermeMachado/AuthLock/AuthLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GuilhermeMachado.AuthLock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
